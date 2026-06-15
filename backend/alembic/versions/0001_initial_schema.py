7ec1c6318127325d0bdf201552082f88942668eb
monerinvest-jpg <moner.invest@gmail.com>
2026-06-15 17:22:19 +0300

    Final

--- backend/alembic/versions/0001_initial_schema.py
+++
@@ -1,9 +0,0 @@
-# Шаг 1: сгенерировать начальную миграцию
-cd backend
-alembic revision --autogenerate -m "initial_schema"
-
-# Шаг 2: применить
-alembic upgrade head
-
-# Шаг 3: проверить что схема актуальна (в CI)
-alembic check   # выдаст ошибку если есть незамигрированные изменения
